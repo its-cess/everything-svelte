c98c6b74484781f41c715a13c9bb332101e4ab34
Cecily Toro <torocecily@gmail.com>
2023-04-16 11:51:23 -0400

    initial setup. add tailwind. add google fonts. add custom colors.

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  useTabs: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  printWidth: 100,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.cjs',
+  pluginSearchDirs: ['.'],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
